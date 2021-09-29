78812ee22bf45160e5fa868328e2dc7d7e513f30
~shouzy~ <82171453+realshouzy@users.noreply.github.com>
2021-09-29 08:25:08 +0200

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,13 +1,4 @@
 #At first: This bot was coded in Replit.
-#About "keep_alive":
-#Its a server to keep the bot online.
-#Make a 2nd file and name it : keep_alive.py
-#Then copy the code from here in that file:
-#The code is from: https://gist.github.com/beaucarnes/51ec37412ab181a2e3fd320ee474b671
-#After that go on uptimerobot and create a new monitor with the url the keep_alive.py file created.
-#Then the bot will stay then online for ever.
-#from flask import Flask
-#from threading import Thread
 
 from flask import Flask
 from threading import Thread
@@ -24,3 +15,12 @@ def run():
 def keep_alive():
     t = Thread(target=run)
     t.start()
+#About "keep_alive":
+#Its a server to keep the bot online.
+#Make a 2nd file and name it : keep_alive.py
+#Then copy the code from here in that file:
+#The code is from: https://gist.github.com/beaucarnes/51ec37412ab181a2e3fd320ee474b671
+#After that go on uptimerobot and create a new monitor with the url the keep_alive.py file created.
+#Then the bot will stay then online for ever.
+#from flask import Flask
+#from threading import Thread
